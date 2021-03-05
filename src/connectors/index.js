95a1df05f6bcaaace8dc22f2cf839f7e34a4f067
Shing Ho Tan <talenteyedev@gmail.com>
2021-03-04 17:05:09 -0800

    initial web3 provider

--- src/connectors/index.js
+++ src/connectors/index.js
@@ -1,5 +1,12 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { NetworkConnector } from "@web3-react/network-connector";
+import { Web3Provider } from '@ethersproject/providers'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+import { WalletLinkConnector } from '@web3-react/walletlink-connector'
+import { PortisConnector } from '@web3-react/portis-connector'
+
+import { FortmaticConnector } from './Fortmatic'
+import { NetworkConnector } from './NetworkConnector'
 
 const POLLING_INTERVAL = 12000;
 const RPC_URLS = {
@@ -7,12 +14,55 @@ const RPC_URLS = {
   4: "https://rinkeby.infura.io/v3/84842078b09946638c03157f83405213",
 };
 
+const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
+const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
+const PORTIS_ID = process.env.REACT_APP_PORTIS_ID
+
+export const NETWORK_CHAIN_ID = parseInt(process.env.REACT_APP_CHAIN_ID ?? '1')
+
+if (typeof NETWORK_URL === 'undefined') {
+  throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`)
+}
+
 export const network = new NetworkConnector({
   urls: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
   defaultChainId: 1,
   pollingInterval: POLLING_INTERVAL,
 });
 
+let networkLibrary
+export function getNetworkLibrary() {
+  return (networkLibrary = networkLibrary ?? new Web3Provider(network.provider))
+}
+
 export const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42],
 });
+
+// mainnet only
+export const walletconnect = new WalletConnectConnector({
+  rpc: { 1: NETWORK_URL },
+  bridge: 'https://bridge.walletconnect.org',
+  qrcode: true,
+  pollingInterval: 15000
+})
+
+// mainnet only
+export const fortmatic = new FortmaticConnector({
+  apiKey: FORMATIC_KEY ?? '',
+  chainId: 1
+})
+
+// mainnet only
+export const portis = new PortisConnector({
+  dAppId: PORTIS_ID ?? '',
+  networks: [1]
+})
+
+// mainnet only
+export const walletlink = new WalletLinkConnector({
+  url: NETWORK_URL,
+  appName: 'Uniswap',
+  appLogoUrl:
+    'https://mpng.pngfly.com/20181202/bex/kisspng-emoji-domain-unicorn-pin-badges-sticker-unicorn-tumblr-emoji-unicorn-iphoneemoji-5c046729264a77.5671679315437924251569.jpg'
+})
