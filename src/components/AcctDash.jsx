d8ad94287936482d21ab73d2c7e7b06a217d079b
Anton <flppv8@gmail.com>
2021-04-03 17:06:00 +0300

    add tablet resolution

--- src/components/AcctDash.jsx
+++ src/components/AcctDash.jsx
@@ -18,16 +18,19 @@ const SidebarLayout = styled.div`
 
   @media screen and (max-width: ${responsive.tablet}) {
     display: grid;
-    grid-template-columns: 70% 30%;
+    grid-template-columns: 1fr 300px;
     grid-template-rows: 100px 100px;
+    justify-content: space-between;
   }
 `;
 
 const Header = styled.header`
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 44px 72px 44px;
+  grid-template-rows: auto auto;
   align-items: center;
-  gap: 32px;
+  justify-content: center;
+  gap: 32px 60px;
 
   height: 240px;
   padding: 40px 48px;
@@ -36,22 +39,33 @@ const Header = styled.header`
   border-radius: 4px;
 
   @media screen and (max-width: ${responsive.laptop}) {
+    grid-template-columns: 32px 56px 32px;
+    gap: 24px 32px;
+
     height: 180px;
     padding: 24px 48px;
   }
 
+  @media screen and (max-width: ${responsive.laptopSmall}) {
+    grid-template-columns: 32px 56px 32px;
+    gap: 24px;
+
+    padding: 24px;
+  }
+
   @media screen and (max-width: ${responsive.tablet}) {
+    grid-template-rows: none;
+    grid-template-columns: 56px 250px 32px 32px;
+    justify-items: start;
+    justify-content: start;
+    gap: 10px;
+
+    height: 100px;
+
     background-color: ${(props) => props.theme.colors.bgNormal};
     border-bottom: 1px solid ${(props) => props.theme.colors.bgHighlightBorder};
+    border-radius: 0;
   }
-`;
-
-const HeaderControls = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  width: 100%;
 `;
 
 const Avatar = styled.img`
@@ -65,6 +79,11 @@ const Avatar = styled.img`
     height: 56px;
     width: 56px;
   }
+
+  @media screen and (max-width: ${responsive.tablet}) {
+    grid-column: 1/2;
+    grid-row: 1/2;
+  }
 `;
 
 const AccountNumber = styled.div`
@@ -72,6 +91,16 @@ const AccountNumber = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 8px;
+
+  grid-column: 1/4;
+
+  @media screen and (max-width: ${responsive.tablet}) {
+    align-items: flex-start;
+    grid-column: 2/3;
+    grid-row: 1/2;
+
+    padding-left: 14px;
+  }
 
   span {
     font-size: 16px;
@@ -197,8 +226,8 @@ const ControlsBtn = styled.a`
   align-items: center;
   justify-content: center;
 
-  height: 40px;
-  width: 40px;
+  height: 44px;
+  width: 44px;
 
   background-color: ${(props) => props.theme.colors.bgDarken};
   border: 1px solid ${(props) => props.theme.colors.bgHighlightBorder};
@@ -214,6 +243,15 @@ const ControlsBtn = styled.a`
     height: 32px;
 
     font-size: 12px;
+  }
+
+  @media screen and (max-width: ${responsive.tablet}) {
+    &:first-child {
+      grid-column: 3/4;
+    }
+    &:nth-child(3) {
+      grid-column: 4/5;
+    }
   }
 `;
 
@@ -249,15 +287,13 @@ export default function AcctDash() {
     <Panel>
       <SidebarLayout>
         <Header>
-          <HeaderControls>
-            <ControlsBtn>
-              <FontAwesomeIcon icon={faCog} />
-            </ControlsBtn>
-            <Avatar src="https://picsum.photos/72" alt="" />
-            <ControlsBtn>
-              <FontAwesomeIcon icon={faSignOutAlt} />
-            </ControlsBtn>
-          </HeaderControls>
+          <ControlsBtn>
+            <FontAwesomeIcon icon={faCog} />
+          </ControlsBtn>
+          <Avatar src="https://picsum.photos/72" alt="" />
+          <ControlsBtn>
+            <FontAwesomeIcon icon={faSignOutAlt} />
+          </ControlsBtn>
           <AccountNumber>
             <p>My Account</p>
             <span>
