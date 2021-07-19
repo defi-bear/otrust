e42533033f00cc70f9e8779efe69e68e751fcbf2
Anton <flppv8@gmail.com>
2021-07-19 23:34:47 +0300

    add tooltip to trimmedwrapper

--- src/components/UI/TrimmedWrapper.jsx
+++ src/components/UI/TrimmedWrapper.jsx
@@ -1,4 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Tooltip = styled.div`
+  display: none;
+  padding: 12px 24px;
+
+  position: absolute;
+  bottom: -48px;
+
+  background-color: ${props => props.theme.colors.bgHighlightBorder};
+  border-radius: 8px;
+  box-shadow: 0 5px 50px 0 rgba(0, 0, 0, 0.36);
+
+  color: ${props => props.theme.colors.textPrimary};
+  font-size: 14px;
+  font-family: 'Poppins', sans-serif;
+
+  z-index: 1;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+
+  &:hover ${Tooltip} {
+    display: ${props => (props.hasValue ? 'block' : 'none')};
+  }
+`;
 
 const formatDecimals = value => {
   if (value === null || value === undefined || value === '') return '';
@@ -21,6 +48,11 @@ export const withTrimmedWrapper = WrappedComponent => {
   return props => {
     const value = formatDecimals(props.value);
 
-    return <WrappedComponent {...props} value={value} />;
+    return (
+      <Wrapper hasValue={!!value}>
+        <Tooltip>{props.value}</Tooltip>
+        <WrappedComponent {...props} value={value} />
+      </Wrapper>
+    );
   };
 };
